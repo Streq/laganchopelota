e0d459da260e3e59fbb11688e1e52f008f9cf0ba
streq <strequena@gmail.com>
2022-11-05 18:29:47 -0300

    no se man que paja, por ahi lo hago shooter top down y sostengo al mecanica de gancho

--- src/pelota/leg.gd
+++ src/pelota/leg.gd
@@ -9,6 +9,9 @@ func _physics_process(delta: float) -> void:
 		dir = input.dir
 	global_rotation = dir.angle()
 	
-	if input.B.is_just_pressed():# and wall_detect.get_overlapping_bodies():
-		owner.apply_central_impulse(dir*jump_speed)
-		
+	if input.B.is_just_pressed() and owner.is_on_wall():
+		var collision_count = owner.get_slide_count()
+		for i in collision_count:
+			var collision : KinematicCollision2D = owner.get_slide_collision(i)
+			owner.apply_central_impulse(collision.normal*jump_speed)
+			return
--- src/pelota/pelota.gd
+++ src/pelota/pelota.gd
@@ -1,8 +1,14 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 onready var input_state := $input_state
 
+export var linear_velocity := Vector2()
+export var gravity := 9.8
+
+
 func _physics_process(delta: float) -> void:
-	pass
-		
-	
+	linear_velocity.y += delta*gravity*25.0
+	linear_velocity = move_and_slide(linear_velocity)
+
+func apply_central_impulse(impulse):
+	linear_velocity += impulse
